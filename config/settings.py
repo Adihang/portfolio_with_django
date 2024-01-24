fa11da16f4b4ab83196eb01d0b6f450fa9571b1b
Adihang <limhan456@naver.com>
2024-01-24 15:41:32 +0900

    ec2 서버 업로드

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+44y678!(-$11*x4^8$-)m(rx0yfkzernw!w^52q2y6s1c740i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [False, '127.0.0.1']
 
 
 # Application definition
