efb774139455ea803647a846b19a23440648c633
HBLim <limhan456@naver.com>
2026-02-16 11:52:51 +0900

    권한관련수정

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import re_path
 from django.views.static import serve
 
@@ -25,8 +26,11 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-
-urlpatterns += [
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+elif getattr(settings, "DJANGO_SERVE_FILES", False):
+    urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
