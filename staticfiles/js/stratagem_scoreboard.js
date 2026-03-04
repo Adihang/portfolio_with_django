0d732469a106439032c60caee967a711783bb6cb
Adihang <limhan456@naver.com>
2026-03-04 11:30:22 +0900

    가상환경 및 staticfiles 파일들 원격 저장소에서 제거

--- staticfiles/js/stratagem_scoreboard.js
+++
@@ -1,6 +0,0 @@
-// DOM이 로드되었을 때 실행되는 초기화 함수
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        location.replace(new URL("../", window.location.href).toString());
-    }, 5000);
-});
